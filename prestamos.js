d16aec8c6b7846d6faa9d183e87361061d381e3e
juanrojas255 <je822900@gmail.com>
2025-10-02 20:31:16 -0500

    juu

--- prestamos.js
+++ prestamos.js
@@ -1,17 +1,20 @@
-import {libros} from "./libros.js"
 
-function cambiarEstado(titulo) {
-    let resultado = libros.filter(libro=> libro.Titulo == titulo)
-    libros[0].Estado = "Prestado";
-    console.log(`El libro "${libros[0].Titulo}" ahora está "Prestado"`)
+import { libros } from "./libros.js"
+
+// prestar un libro
+export function cambiarEstado(titulo) {
+    let resultado = libros.filter(libro => libro.Titulo == titulo)
+    resultado[0].Estado = "Prestado"
+    console.log(`El libro "${titulo}" ahora está Prestado`)
 }
 
-
-function devolverLibro(titulo) {
+// devolver un libro
+export function devolverLibro(titulo) {
     let resultado = libros.filter(libro => libro.Titulo == titulo)
     resultado[0].Estado = "Disponible"
     console.log(`El libro "${titulo}" ahora está Disponible`)
 }
 
-cambiarEstado("guerra y paz")
-devolverLibro("cien años de soledad")
+
+cambiarEstado("Guerra y paz")
+devolverLibro("Cien años de soledad")
