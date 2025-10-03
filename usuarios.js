739bdb4f8732d6a76179e0f827f6faf495cfb5db
Nicole <nxicmp0510@gmail.com>
2025-10-02 19:20:35 -0500

    commit F

--- usuarios.js
+++ usuarios.js
@@ -2,35 +2,24 @@
 //Nombre
 //Correo electrónico
 
-let usuarios = [];
+let usuarios = []
 
 function registrarUsuarios(nombre, correo) {
    
     let usuario = {
         nombre: nombre,
         correo: correo
-    };
+    }
 
   
-    usuarios.push(usuario);
+    usuarios.push(usuario)
 
-    console.log("Usuario registrado:", usuario);
+    console.log("Usuario registrado:", usuario)
 }
 
-registrarUsuarios("Juan Perez", "juan@gmail.com");
-registrarUsuarios("Ana Lopez", "ana@gmail.com");
+registrarUsuarios("Juan Perez", "juan@gmail.com")
+registrarUsuarios("Ana Lopez", "ana@gmail.com")
 
-console.log(usuarios);
+console.log(usuarios)
 
-
-//registrar un libro = find.index
-
-//Funcion para Prestar y devolver un libro:
-//Solo si está disponible.
-//Cambiar el estado a prestado.
-
-
-function prestarLibro() {
-
-}
 
